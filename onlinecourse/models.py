c4c4fd28b78de19bc9e9a12c1b174e3ea8319321
teklily <lilianaweb@teklily.com>
2022-10-28 20:29:47 -0700

    updates for lab

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 28 18:40:28 2022
+
+@author: Lili
+"""
+
 import sys
 from django.utils.timezone import now
 try:
@@ -101,20 +108,26 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+class Question(models.Model):
     # Foreign key to lesson
+    lesson = models.ForeignKey(Lesson , on_delete=models.CASCADE)
     # question text
+    question_text = models.TextField()
     # question grade/mark
-
+    grade = models.FloatField()
+    # question grade/mark
+    
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+def is_get_score(self, selected_ids):
+    all_answers = self.choice_set.filter(is_correct=True).count()
+    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+    if all_answers == selected_correct:
+        return True
+    else:
+        return False
 
+    def __str__(self):
+        return self.question_text
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
@@ -122,13 +135,21 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.choice_text
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
-#    Other fields and methods you would like to design
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+   # Other fields and methods you would like to design
+    
+    def __str__(self):
+        return f"submission:{self.pk}"
